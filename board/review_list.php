419b12d6e5619fba18c86665dddc05845214382f
김재홍 <jjaekong@gmail.com>
2016-03-03 19:17:43 +0900

    board > review list 작업 완료

--- board/review_list.php
+++ board/review_list.php
@@ -15,7 +15,7 @@
                 <span></span>
                 <span></span>
             </div>
-            <div class="rank">
+            <div class="reward">
                 <ol>
                     <li><img src="/design/wonderbra/images/sub/img_review_rank_1.gif" alt="1등 10만원"></li>
                     <li><img src="/design/wonderbra/images/sub/img_review_rank_2.gif" alt="2등 7만원"></li>
@@ -39,13 +39,210 @@
                                 <img src="/design/wonderbra/images/dummy/img_qna_thumb.jpg">
                                 <figcaption>
                                     <strong>원더브라 PERFECT PUSH-UP 2종세트</strong>
-                                    <span>4.5</span>
+                                    <p class="rating">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    </p>
+                                </figcaption>
+                            </figure>
+                        </a>
+                    </li>
+                    <li class="col-xs-3">
+                        <a class="review" href="#">
+                            <figure>
+                                <img src="/design/wonderbra/images/dummy/img_product_3.jpg">
+                                <figcaption>
+                                    <strong>같은 상품 재구매 했어요!</strong>
+                                    <small>wonderbra</small>
+                                </figcaption>
+                            </figure>
+                        </a>
+                        <a class="product" href="#">
+                            <figure>
+                                <img src="/design/wonderbra/images/dummy/img_qna_thumb.jpg">
+                                <figcaption>
+                                    <strong>원더브라 PERFECT PUSH-UP 2종세트</strong>
+                                    <p class="rating">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    </p>
+                                </figcaption>
+                            </figure>
+                        </a>
+                    </li>
+                    <li class="col-xs-3">
+                        <a class="review" href="#">
+                            <figure>
+                                <img src="/design/wonderbra/images/dummy/img_product_1.jpg">
+                                <figcaption>
+                                    <strong>같은 상품 재구매 했어요!</strong>
+                                    <small>wonderbra</small>
+                                </figcaption>
+                            </figure>
+                        </a>
+                        <a class="product" href="#">
+                            <figure>
+                                <img src="/design/wonderbra/images/dummy/img_qna_thumb.jpg">
+                                <figcaption>
+                                    <strong>원더브라 PERFECT PUSH-UP 2종세트</strong>
+                                    <p class="rating">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    </p>
+                                </figcaption>
+                            </figure>
+                        </a>
+                    </li>
+                    <li class="col-xs-3">
+                        <a class="review" href="#">
+                            <figure>
+                                <img src="/design/wonderbra/images/dummy/img_product_2.jpg">
+                                <figcaption>
+                                    <strong>같은 상품 재구매 했어요!</strong>
+                                    <small>wonderbra</small>
+                                </figcaption>
+                            </figure>
+                        </a>
+                        <a class="product" href="#">
+                            <figure>
+                                <img src="/design/wonderbra/images/dummy/img_qna_thumb.jpg">
+                                <figcaption>
+                                    <strong>원더브라 PERFECT PUSH-UP 2종세트</strong>
+                                    <p class="rating">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                        <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    </p>
                                 </figcaption>
                             </figure>
                         </a>
                     </li>
                 </ul>
             </div>
+            <div class="search">
+                <form class="form-inline" action="#" method="post">
+                    <div class="form-group">
+                        <select class="form-control" name="">
+                            <option value="#">전체</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label class="sr-only" for="board-keyword">검색어</label>
+                        <input type="text" class="form-control" id="board-keyword">
+                    </div>
+                    <button type="submit" class="btn btn-default">
+                        <span class="glyphicon glyphicon-search"></span>
+                        <span class="sr-only">검색</span>
+                    </button>
+                </form>
+            </div>
+            <div class="review-list">
+                <table class="table">
+                    <colgroup>
+                        <col width="150">
+                        <col>
+                        <col width="170">
+                        <col width="130">
+                    </colgroup>
+                    <tbody>
+                        <tr>
+                            <td><img src="/design/wonderbra/images/dummy/img_review_1.gif" alt=""></td>
+                            <td class="title">
+                                <a href="#"><strong>원더브라 Sexy Wirefree 베이지 2종세트 WW2D51_T</strong></a>
+                                <img src="/design/wonderbra/images/sub/ico_new.gif" alt="새글">
+                                <p class="summary">같은상품 재구매 했어요!</p>
+                                <p class="details">디자인도 예쁘고 편안해서 재구매 진행했습니다^^ 노와이어도 역시 원더브라네요~~~</p>
+                            </td>
+                            <td>
+                                원더브라
+                                <p class="rating">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                </p>
+                            </td>
+                            <td><img src="/design/wonderbra/images/dummy/img_review_2.gif" alt=""></td>
+                        </tr>
+                        <tr>
+                            <td><img src="/design/wonderbra/images/dummy/img_review_1.gif" alt=""></td>
+                            <td class="title">
+                                <a href="#"><strong>원더브라 Sexy Wirefree 베이지 2종세트 WW2D51_T</strong></a>
+                                <img src="/design/wonderbra/images/sub/ico_new.gif" alt="새글">
+                                <p class="summary">같은상품 재구매 했어요!</p>
+                                <p class="details">디자인도 예쁘고 편안해서 재구매 진행했습니다^^ 노와이어도 역시 원더브라네요~~~</p>
+                            </td>
+                            <td>
+                                원더브라
+                                <p class="rating">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                </p>
+                            </td>
+                            <td><img src="/design/wonderbra/images/dummy/img_review_2.gif" alt=""></td>
+                        </tr>
+                        <tr>
+                            <td><img src="/design/wonderbra/images/dummy/img_review_1.gif" alt=""></td>
+                            <td class="title">
+                                <a href="#"><strong>원더브라 Sexy Wirefree 베이지 2종세트 WW2D51_T</strong></a>
+                                <img src="/design/wonderbra/images/sub/ico_new.gif" alt="새글">
+                                <p class="summary">같은상품 재구매 했어요!</p>
+                                <p class="details">디자인도 예쁘고 편안해서 재구매 진행했습니다^^ 노와이어도 역시 원더브라네요~~~</p>
+                            </td>
+                            <td>
+                                원더브라
+                                <p class="rating">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                    <img src="/design/wonderbra/images/sub/ico_star.gif" alt="">
+                                </p>
+                            </td>
+                            <td><img src="/design/wonderbra/images/dummy/img_review_2.gif" alt=""></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div class="btn-area">
+                    <p>
+                        <a href="#" class="btn btn-default">글쓰기</a>
+                    </p>
+                </div>
+            </div>
+            <nav class="paging">
+                <ul class="pagination">
+                    <li>
+                        <a href="#" aria-label="이전">
+                            <span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
+                        </a>
+                    </li>
+                    <li class="active"><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li>
+                        <a href="#" aria-label="다음">
+                            <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </main>
     <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>

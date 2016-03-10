3c5dc8e24de2576c539c3a223507e58f7a226487
김재홍 <jjaekong@gmail.com>
2016-03-10 12:23:00 +0900

    마이페이지 > 1:1문의 뷰: 작업완료

---
+++ mypage/inquiry_view.php
@@ -0,0 +1,98 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+</head>
+<body class="sub">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <main id="content" tabindex="-1" class="mypage inquiry board">
+        <div class="page-header">
+            <div class="container">
+                <h2 class="page-title">1:1 문의</h2>
+            </div>
+        </div>
+        <div class="container">
+            <div class="category">
+                <ul class="nav nav-pills">
+                    <li role="presentation"><a href="#">주문내역/배송조회</a></li>
+                    <li role="presentation"><a href="#">취소/교환/환불 조회</a></li>
+                    <li role="presentation"><a href="#">적립금 내역</a></li>
+                    <li role="presentation"><a href="#">할인쿠폰 내역</a></li>
+                    <li role="presentation"><a href="#">B-money 내역</a></li>
+                    <li role="presentation"><a href="#">쿠폰등록</a></li>
+                    <li role="presentation" class="br"></li>
+                    <li role="presentation"><a href="#">구매후기</a></li>
+                    <li role="presentation"><a href="#">위시리스트</a></li>
+                    <li role="presentation"><a href="#">최근 본 상품목록</a></li>
+                    <li role="presentation"><a href="#">상품 Q&amp;A</a></li>
+                    <li role="presentation" class="active"><a href="#">1:1문의</a></li>
+                    <li role="presentation"><a href="#">회원정보수정</a></li>
+                    <li role="presentation"><a href="#">회원탈퇴</a></li>
+                </ul>
+            </div>
+            <div class="inquiry-view">
+                <table class="table">
+                    <colgroup>
+                        <col style="width: 200px;">
+                        <col>
+                    </colgroup>
+                    <tbody>
+                        <tr class="title">
+                            <th>제목</th>
+                            <td><strong>배송문의합니다.</strong></td>
+                        </tr>
+                        <tr>
+                            <th>이메일</th>
+                            <td>test@mkdesign.co.kr</td>
+                        </tr>
+                        <tr>
+                            <th>핸드폰</th>
+                            <td>010-1234-5678</td>
+                        </tr>
+                        <tr>
+                            <th>답변</th>
+                            <td>답변이 작성되었습니다.</td>
+                        </tr>
+                        <tr class="question">
+                            <th>내용</th>
+                            <td>
+                                7일에 주문했는데 언제 받을수있나요?<br>
+                                주문내역 확인했는데 입금완료로 뜨는데 아직 배송 안된거죠?<br>
+                                빠른 배송 및 확인 부탁 드립니다 ~~ <br>
+                            </td>
+                        </tr>
+                        <tr class="answer">
+                            <th>질문답변</th>
+                            <td>
+                                안녕하세요~ 고객님!<br>
+                                주문 상품 금일 출고 되었습니다. <br>
+                                <br>
+                                고객님의 기대에 어긋나지 않게 항상 노력하고, <br>
+                                늘 즐거운 쇼핑이 되실수 있도록 최선을 다하겠습니다.<br>
+                                <br>
+                                다른문의사항이 있으실경우 <br>
+                                 고객센터 또는 1:1문의 게시판으로 접수부탁드립니다.<br>
+                                감사합니다. <br>
+                                <br>
+                                 [고객센터 1544-6101 이용안내]<br>
+                                <br>
+                                평 일 : 10:00~17:00 <br>
+                                점 심 : 11:40~13:00 <br>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <ul class="util">
+                    <li class="edit"><a href="#">수정</a></li>
+                    <li class="delete"><a href="#">삭제</a></li>
+                    <li class="reply"><a href="#">답변</a></li>
+                </ul>
+                <div class="btn-area">
+                    <p>
+                        <a href="#" class="btn btn-default btn-list">목록</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>

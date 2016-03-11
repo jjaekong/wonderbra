5a219ef5003300bed5b9e33c86073b53db64dc19
김재홍 <jjaekong@gmail.com>
2016-03-11 15:01:42 +0900

    제이쿼리 msie 사용문제 임시해결

--- inc/docfoot.php
+++ inc/docfoot.php
@@ -1,3 +1,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="/design/wonderbra/js/bootstrap.min.js"></script>
 <script src="/design/wonderbra/js/common.js"></script>
+<script src="/design/wonderbra/js/jquery.browser.js"></script>

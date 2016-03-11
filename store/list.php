66996b9c6035d1ecb8c9182d1e1eda00b50a8d6c
김재홍 <jjaekong@gmail.com>
2016-03-11 18:35:50 +0900

    스토어 목록 작업완료!

---
+++ store/list.php
@@ -0,0 +1,107 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+</head>
+<body class="sub">
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <main id="content" tabindex="-1" class="store">
+        <div class="page-header">
+            <div class="container">
+                <h2 class="page-title">STORE</h2>
+            </div>
+        </div>
+        <div class="container">
+            <div class="store-list">
+                <table class="table">
+                    <colgroup>
+                        <col style="width: 200px">
+                        <col>
+                        <col style="width: 150px">
+                    </colgroup>
+                    <tbody>
+                        <tr>
+                            <th>원더브라 강남점</th>
+                            <td>
+                                <address>서울특별시 강남구 강남대로 390 (지번 서울특별시 강남구 역삼동 825)</address>
+                                <dl>
+                                    <dt>전화번호</dt>
+                                    <dd>02-555-1135</dd>
+                                    <dt>영업시간</dt>
+                                    <dd>10:00 ~ 22:00</dd>
+                                </dl>
+                            </td>
+                            <td>
+                                <a href="#" class="btn btn-default btn-details">상세보기</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>원더브라 강남점</th>
+                            <td>
+                                <address>서울특별시 강남구 강남대로 390 (지번 서울특별시 강남구 역삼동 825)</address>
+                                <dl>
+                                    <dt>전화번호</dt>
+                                    <dd>02-555-1135</dd>
+                                    <dt>영업시간</dt>
+                                    <dd>10:00 ~ 22:00</dd>
+                                </dl>
+                            </td>
+                            <td>
+                                <a href="#" class="btn btn-default btn-details">상세보기</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>원더브라 강남점</th>
+                            <td>
+                                <address>서울특별시 강남구 강남대로 390 (지번 서울특별시 강남구 역삼동 825)</address>
+                                <dl>
+                                    <dt>전화번호</dt>
+                                    <dd>02-555-1135</dd>
+                                    <dt>영업시간</dt>
+                                    <dd>10:00 ~ 22:00</dd>
+                                </dl>
+                            </td>
+                            <td>
+                                <a href="#" class="btn btn-default btn-details">상세보기</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>원더브라 강남점</th>
+                            <td>
+                                <address>서울특별시 강남구 강남대로 390 (지번 서울특별시 강남구 역삼동 825)</address>
+                                <dl>
+                                    <dt>전화번호</dt>
+                                    <dd>02-555-1135</dd>
+                                    <dt>영업시간</dt>
+                                    <dd>10:00 ~ 22:00</dd>
+                                </dl>
+                            </td>
+                            <td>
+                                <a href="#" class="btn btn-default btn-details">상세보기</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <nav class="paging">
+                <ul class="pagination">
+                    <li>
+                        <a href="#" aria-label="이전">
+                            <span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
+                        </a>
+                    </li>
+                    <li class="active"><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li>
+                        <a href="#" aria-label="다음">
+                            <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+</body>
+</html>

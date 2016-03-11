6f47399373cd2d0e64efc1004e30b9e2b62d0bd6
김재홍 <jjaekong@gmail.com>
2016-03-11 19:52:34 +0900

    온에어 NOW 작업완료!

--- onair/now.php
+++ onair/now.php
@@ -5,45 +5,16 @@
     <main id="content" tabindex="-1" class="onair now">
         <div class="container">
             <div class="broadcast">
-                <div class="row">
-                    <div class="col-xs-6">
-                        <h2 class="page-title">ON AIR</h2>
-                    </div>
-                    <div class="clearfix"></div>
-                    <div class="col-xs-6">
-                        <div class="embed-wrap">
-                            <iframe src="https://player.vimeo.com/video/21106804?title=0&byline=0&portrait=0" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-                        </div>
-                    </div>
-                    <div class="col-xs-6">
-                        <time class="countdown">02:13:42</time>
-                        <h2 class="product-title">원더브라 WONDER BOOST 브라패키지 22종세트</h2>
-                        <dl class="price">
-                            <dt>고객맞춤가</dt>
-                            <dd>169,000 won</dd>
-                        </dl>
-                        <div class="summary">
-                            <p>
-                                원더브라 WONDER BOOST 10종세트 (브라5 + 팬티 5)<br>
-                                추가구성: 나시탑2종세트 + 원더코튼팬티3종 + 시크릿팬티3종+선글라스 + 어깨끈
-                            </p>
-                        </div>
-                        <div class="details">
-                            <p>
-                                원더브라만의 진화된 업그레이드 원더 부스트 (UPGRADE WONDER BOOST)<br>
-                                1.45도 입체 볼륨 각도로 모아주고 (PUSH IN)내장형 오일패드로 올려주고 (PUSH UP)<br>
-                                2.원더브라만의 부스트 패드와 원더 겔이 가슴을 더블 푸쉬업 해주고 입체감있는 바디라인 연출
-                            </p>
-                        </div>
-                        <div class="btn-area">
-                            <p>
-                                <a class="btn btn-default btn-lg" href="#">BUY NOW</a>
-                                <a class="btn btn-default btn-lg" href="#">CART</a>
-                                <a class="btn btn-default btn-lg" href="#">WISH LIST</a>
-                            </p>
-                        </div>
-                    </div>
+                <h2 class="page-title">ON AIR <time id="countdown">02:13:42</time></h2>
+                <div class="embed-wrap">
+                    <iframe src="https://player.vimeo.com/video/94607402?byline=0&portrait=0" width="670" height="377" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                 </div>
+            </div>
+            <div class="product">
+                <p><a href="#"><img src="/design/wonderbra/images/dummy/img_onair_product.gif" alt="방송동일구성! 원더브라 WONDER BOOST 브라패키지 22종세트"></a></p>
+            </div>
+            <div class="added">
+                <p><img src="/design/wonderbra/images/sub/img_overview_1.jpg" alt=""></p>
             </div>
         </div>
     </main>

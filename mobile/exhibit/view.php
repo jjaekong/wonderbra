96765776ebef74546dfd60b832d474c0c48f4313
Kim Jaehong <jjaekong@gmail.com>
2016-04-03 19:58:18 +0900

    모바일: 기획전, 온에어 > NOW 작업 완료

---
+++ mobile/exhibit/view.php
@@ -0,0 +1,37 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/dochead.php'); ?>
+</head>
+<body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
+    <main id="content" class="sub exhibit">
+        <div class="exhibit-visual">
+            <p><img class="img-responsive" src="/design/wonderbra/mobile/images/img_exhibit_visual.jpg" alt=""></p>
+        </div>
+        <div class="exhibit-list">
+            <ul class="row">
+                <li class="col-xs-12">
+                    <a href="#">
+                        <img class="img-responsive" src="/design/wonderbra/mobile/images/img_exhibit_item.jpg" alt="">
+                    </a>
+                </li>
+                <li class="col-xs-12">
+                    <a href="#">
+                        <img class="img-responsive" src="/design/wonderbra/mobile/images/img_exhibit_item.jpg" alt="">
+                    </a>
+                </li>
+                <li class="col-xs-12">
+                    <a href="#">
+                        <img class="img-responsive" src="/design/wonderbra/mobile/images/img_exhibit_item.jpg" alt="">
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="exhibit-banner">
+            <p>
+                <a href="#"><img class="img-responsive" src="/design/wonderbra/mobile/images/img_exhibit_banner.jpg" alt=""></a>
+            </p>
+        </div>
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
+</body>
+</html>
---
+++ mobile/onair/now.php
@@ -0,0 +1,99 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/dochead.php'); ?>
+</head>
+<body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
+    <main id="content" class="sub onair now">
+        <section class="cast">
+            <div class="page-header">
+                <h2 class="title">ON AIR</h2>
+                <hr>
+                <p class="count">
+                    02:13:40
+                </p>
+            </div>
+            <div class="embed-responsive embed-responsive-4by3">
+                <iframe src="https://player.vimeo.com/video/11187197?title=0&byline=0&portrait=0" width="500" height="338" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+            </div>
+            <div class="banner">
+                <p><a href="#"><img class="img-responsive" src="/design/wonderbra/mobile/images/img_onair_product.jpg" alt=""></a></p>
+            </div>
+        </section>
+        <section class="best">
+            <div class="page-header">
+                <h2 class="title">ON AIR BEST</h2>
+            </div>
+            <div class="container">
+                <div class="product-list">
+                    <ul class="row">
+                        <li class="col-xs-6">
+                            <a href="#">
+                                <figure>
+                                    <img class="img-responsive" src="/design/wonderbra/mobile/images/dummy/img_product_1.jpg" alt="">
+                                    <figcaption>
+                                        <p class="title">[Wonderbra] 원더브라 Sexy Non-Wire 네이비 2종세트</p>
+                                        <p class="price"><strong>67,000</strong></p>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-xs-6">
+                            <a href="#">
+                                <figure>
+                                    <img class="img-responsive" src="/design/wonderbra/mobile/images/dummy/img_product_2.jpg" alt="">
+                                    <figcaption>
+                                        <p class="title">[Wonderbra] 원더브라 Sexy Non-Wire 네이비 2종세트</p>
+                                        <p class="price"><strong>67,000</strong></p>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-xs-6">
+                            <a href="#">
+                                <figure>
+                                    <img class="img-responsive" src="/design/wonderbra/mobile/images/dummy/img_product_3.jpg" alt="">
+                                    <figcaption>
+                                        <p class="title">[Wonderbra] 원더브라 Sexy Non-Wire 네이비 2종세트</p>
+                                        <p class="price"><strong>67,000</strong></p>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                        <li class="col-xs-6">
+                            <a href="#">
+                                <figure>
+                                    <img class="img-responsive" src="/design/wonderbra/mobile/images/dummy/img_product_4.jpg" alt="">
+                                    <figcaption>
+                                        <p class="title">[Wonderbra] 원더브라 Sexy Non-Wire 네이비 2종세트</p>
+                                        <p class="price"><strong>67,000</strong></p>
+                                    </figcaption>
+                                </figure>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <nav class="paging">
+                    <ul class="pagination">
+                        <li>
+                            <a href="#" aria-label="Previous">
+                                <span class="glyphicon glyphicon-triangle-left" aria-hidden="true"></span>
+                            </a>
+                        </li>
+                        <li class="active"><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                        <li><a href="#">4</a></li>
+                        <li><a href="#">5</a></li>
+                        <li>
+                            <a href="#" aria-label="Next">
+                                <span class="glyphicon glyphicon-triangle-right" aria-hidden="true"></span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </section>
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
+</body>
+</html>

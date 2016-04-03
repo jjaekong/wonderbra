4ca5a15d5940b560b655aca0aee85d643c7e1485
Kim Jaehong <jjaekong@gmail.com>
2016-04-03 19:57:56 +0900

    모바일: 기획전, 온에어 > now 작업완료

--- mobile/inc/dochead.php
+++ mobile/inc/dochead.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>원더브라 전용 쇼핑몰</title>
-    <link href='https://fonts.googleapis.com/css?family=Maven+Pro:400,500,700|Noto+Sans|Abril+Fatface' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Maven+Pro:400,500,700|Noto+Sans|Abril+Fatface|Roboto:100' rel='stylesheet' type='text/css'>
     <link href="/design/wonderbra/mobile/css/bootstrap.min.css" rel="stylesheet">
     <link href="/design/wonderbra/mobile/css/style.css" rel="stylesheet">
     <!--[if lt IE 9]>

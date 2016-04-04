e3590f739954e38cfe3a2c4d32649d1161bf8a11
김재홍 <jjaekong@gmail.com>
2016-04-04 19:08:49 +0900

    모바일: 주문상세내역 완료

---
+++ mobile/mypage/order_view.php
@@ -0,0 +1,347 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/dochead.php'); ?>
+</head>
+<body>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/header.php'); ?>
+    <main id="content" class="sub mypage order">
+        <div class="page-header">
+            <h2 class="title">주문내역/배송조회</h2>
+        </div>
+        <div class="container">
+            <div class="order-view">
+                <article class="oder-details">
+                    <header>
+                        <h4>주문번호[42193-8291389-210321]</h4>
+                    </header>
+                    <section class="product">
+                        <h5>원더브라 Sexy Wirefree 베이지 2종세트 WW2D51_T</h5>
+                        <dl>
+                            <dt>수량</dt>
+                            <dd>1</dd>
+                        </dl>
+                        <a href="#" class="btn btn-default btn-review">리뷰작성</a>
+                    </section>
+                    <section class="delivery">
+                        <header>
+                            <h5>배송상세 정보</h5>
+                            <button type="button" class="btn btn-default btn-details">열기</button>
+                        </header>
+                        <table class="table">
+                            <colgroup>
+                                <col style="width: 80px;">
+                                <col>
+                            </colgroup>
+                            <tbody>
+                                <tr>
+                                    <th>배송번호</th>
+                                    <td>S-fdsakjfsd-a0fdsakfkldsa</td>
+                                </tr>
+                                <tr>
+                                    <th>받으실 분</th>
+                                    <td>테스트</td>
+                                </tr>
+                                <tr>
+                                    <th>우편번호</th>
+                                    <td>05763</td>
+                                </tr>
+                                <tr>
+                                    <th>주소</th>
+                                    <td>서울 서초구 양재천로</td>
+                                </tr>
+                                <tr>
+                                    <th>연락처1</th>
+                                    <td>010-1234-5678</td>
+                                </tr>
+                                <tr>
+                                    <th>연락처2</th>
+                                    <td>010-1234-5678</td>
+                                </tr>
+                                <tr>
+                                    <th>주문메시지</th>
+                                    <td>부재시 경비실에 맡겨주세요.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </section>
+                    <section class="payment">
+                        <header>
+                            <h5>결제/주문 정보</h5>
+                            <button type="button" class="btn btn-default btn-details">열기</button>
+                        </header>
+                        <table class="table">
+                            <colgroup>
+                                <col style="width: 80px;">
+                                <col>
+                            </colgroup>
+                            <tbody>
+                                <tr>
+                                    <th>결제금액</th>
+                                    <td>79,000원</td>
+                                </tr>
+                                <tr>
+                                    <th>받으실 분</th>
+                                    <td>사용: 0원</td>
+                                </tr>
+                                <tr>
+                                    <th>결제방식</th>
+                                    <td>무통장입금(기업은행 [가상] 3281903920180 테스트)</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table class="table">
+                            <colgroup>
+                                <col>
+                                <col style="width: 70px;">
+                                <col style="width: 70px;">
+                            </colgroup>
+                            <thead>
+                                <tr>
+                                    <th>결제관련내역</th>
+                                    <th>금액</th>
+                                    <th>적립</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>[wonderbra] 원더브라 full effect 브라팬티 11종 세트 </td>
+                                    <td>-5,000원</td>
+                                    <td>0</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div class="btn-area">
+                            <p>
+                                <a href="#" class="btn btn-default btn-cancel btn-block">주문취소</a>
+                            </p>
+                        </div>
+                    </section>
+                </article>
+                <article class="oder-details">
+                    <header>
+                        <h4>주문번호[42193-8291389-210321]</h4>
+                    </header>
+                    <section class="product">
+                        <h5>원더브라 Sexy Wirefree 베이지 2종세트 WW2D51_T</h5>
+                        <dl>
+                            <dt>수량</dt>
+                            <dd>1</dd>
+                        </dl>
+                        <a href="#" class="btn btn-default btn-review">리뷰작성</a>
+                    </section>
+                    <section class="delivery">
+                        <header>
+                            <h5>배송상세 정보</h5>
+                            <button type="button" class="btn btn-default btn-details">열기</button>
+                        </header>
+                        <table class="table">
+                            <colgroup>
+                                <col style="width: 80px;">
+                                <col>
+                            </colgroup>
+                            <tbody>
+                                <tr>
+                                    <th>배송번호</th>
+                                    <td>S-fdsakjfsd-a0fdsakfkldsa</td>
+                                </tr>
+                                <tr>
+                                    <th>받으실 분</th>
+                                    <td>테스트</td>
+                                </tr>
+                                <tr>
+                                    <th>우편번호</th>
+                                    <td>05763</td>
+                                </tr>
+                                <tr>
+                                    <th>주소</th>
+                                    <td>서울 서초구 양재천로</td>
+                                </tr>
+                                <tr>
+                                    <th>연락처1</th>
+                                    <td>010-1234-5678</td>
+                                </tr>
+                                <tr>
+                                    <th>연락처2</th>
+                                    <td>010-1234-5678</td>
+                                </tr>
+                                <tr>
+                                    <th>주문메시지</th>
+                                    <td>부재시 경비실에 맡겨주세요.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </section>
+                    <section class="payment">
+                        <header>
+                            <h5>결제/주문 정보</h5>
+                            <button type="button" class="btn btn-default btn-details">열기</button>
+                        </header>
+                        <table class="table">
+                            <colgroup>
+                                <col style="width: 80px;">
+                                <col>
+                            </colgroup>
+                            <tbody>
+                                <tr>
+                                    <th>결제금액</th>
+                                    <td>79,000원</td>
+                                </tr>
+                                <tr>
+                                    <th>받으실 분</th>
+                                    <td>사용: 0원</td>
+                                </tr>
+                                <tr>
+                                    <th>결제방식</th>
+                                    <td>무통장입금(기업은행 [가상] 3281903920180 테스트)</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table class="table">
+                            <colgroup>
+                                <col>
+                                <col style="width: 70px;">
+                                <col style="width: 70px;">
+                            </colgroup>
+                            <thead>
+                                <tr>
+                                    <th>결제관련내역</th>
+                                    <th>금액</th>
+                                    <th>적립</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>[wonderbra] 원더브라 full effect 브라팬티 11종 세트 </td>
+                                    <td>-5,000원</td>
+                                    <td>0</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div class="btn-area">
+                            <p>
+                                <a href="#" class="btn btn-default btn-cancel btn-block">주문취소</a>
+                            </p>
+                        </div>
+                    </section>
+                </article>
+                <article class="oder-details">
+                    <header>
+                        <h4>주문번호[42193-8291389-210321]</h4>
+                    </header>
+                    <section class="product">
+                        <h5>원더브라 Sexy Wirefree 베이지 2종세트 WW2D51_T</h5>
+                        <dl>
+                            <dt>수량</dt>
+                            <dd>1</dd>
+                        </dl>
+                        <a href="#" class="btn btn-default btn-review">리뷰작성</a>
+                    </section>
+                    <section class="delivery">
+                        <header>
+                            <h5>배송상세 정보</h5>
+                            <button type="button" class="btn btn-default btn-details">열기</button>
+                        </header>
+                        <table class="table">
+                            <colgroup>
+                                <col style="width: 80px;">
+                                <col>
+                            </colgroup>
+                            <tbody>
+                                <tr>
+                                    <th>배송번호</th>
+                                    <td>S-fdsakjfsd-a0fdsakfkldsa</td>
+                                </tr>
+                                <tr>
+                                    <th>받으실 분</th>
+                                    <td>테스트</td>
+                                </tr>
+                                <tr>
+                                    <th>우편번호</th>
+                                    <td>05763</td>
+                                </tr>
+                                <tr>
+                                    <th>주소</th>
+                                    <td>서울 서초구 양재천로</td>
+                                </tr>
+                                <tr>
+                                    <th>연락처1</th>
+                                    <td>010-1234-5678</td>
+                                </tr>
+                                <tr>
+                                    <th>연락처2</th>
+                                    <td>010-1234-5678</td>
+                                </tr>
+                                <tr>
+                                    <th>주문메시지</th>
+                                    <td>부재시 경비실에 맡겨주세요.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </section>
+                    <section class="payment">
+                        <header>
+                            <h5>결제/주문 정보</h5>
+                            <button type="button" class="btn btn-default btn-details">열기</button>
+                        </header>
+                        <table class="table">
+                            <colgroup>
+                                <col style="width: 80px;">
+                                <col>
+                            </colgroup>
+                            <tbody>
+                                <tr>
+                                    <th>결제금액</th>
+                                    <td>79,000원</td>
+                                </tr>
+                                <tr>
+                                    <th>받으실 분</th>
+                                    <td>사용: 0원</td>
+                                </tr>
+                                <tr>
+                                    <th>결제방식</th>
+                                    <td>무통장입금(기업은행 [가상] 3281903920180 테스트)</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table class="table">
+                            <colgroup>
+                                <col>
+                                <col style="width: 70px;">
+                                <col style="width: 70px;">
+                            </colgroup>
+                            <thead>
+                                <tr>
+                                    <th>결제관련내역</th>
+                                    <th>금액</th>
+                                    <th>적립</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>[wonderbra] 원더브라 full effect 브라팬티 11종 세트 </td>
+                                    <td>-5,000원</td>
+                                    <td>0</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div class="btn-area">
+                            <p>
+                                <a href="#" class="btn btn-default btn-cancel btn-block">주문취소</a>
+                            </p>
+                        </div>
+                    </section>
+                </article>
+            </div>
+        </div>
+    </main>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/mobile/inc/docfoot.php'); ?>
+    <script>
+        (function($) {
+            $(document).on('click', '.btn-details', function() {
+                $(this).parents('section:eq(0)').find('> .table').toggle();
+                $(this).parents('section:eq(0)').find('> .btn-area').toggle();
+                return;                
+            });
+        })(jQuery);
+    </script>
+</body>
+</html>
